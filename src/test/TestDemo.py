a799c685508429628a788c5cbfe22b3972c320d0
ys10 <1269273556@qq.com>
2017-02-22 10:58:27 +0800

    Update the TestDemo.

--- src/test/TestDemo.py
+++ src/test/TestDemo.py
@@ -2,5 +2,13 @@ import tensorflow as tf
 
 # Ensure you have install TensorFlow correctly.
 hello = tf.constant('Hello, TensorFlow!')
+states = tf.zeros(shape=[1,2])
+variable = tf.Variable(states)
+print(states)
+# Start a TensorFlow session.
 sess = tf.Session()
 print(sess.run(hello))
+print(sess.run(states))
+# Initialize variable.
+sess.run(tf.global_variables_initializer())
+print(sess.run(variable))
