fa78d865cdcdb571a8fa6c86007ade5dbf675da0
ys10 <1269273556@qq.com>
2017-02-22 10:57:21 +0800

    Update the LSTMDemo.

--- src/lstm/LSTMDemo.py
+++ src/lstm/LSTMDemo.py
@@ -8,12 +8,12 @@ output_size = 46
 # Layer number of NN model.
 layers_num = 2
 
-# Data config.
-batch_size = 10
-
 # Construct LSTM layers.
 lstm_cell = tf.contrib.rnn.BasicLSTMCell(lstm_size, forget_bias=0.0, state_is_tuple=True)
 cell = tf.contrib.rnn.MultiRNNCell([lstm_cell] * layers_num, state_is_tuple=True)
+
+# Data config.
+batch_size = 10
 
 # Initial states of LSTM.
 initial_state = cell.zero_state(batch_size, tf.float32)
@@ -22,8 +22,11 @@ initial_state = cell.zero_state(batch_size, tf.float32)
 # TODO
 
 # Train the Network with data.
+# Initialize the loss.
+loss = 0;
 # Process training data by Trunk.
 # TODO
+
 
 
 
