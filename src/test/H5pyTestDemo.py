124adc0988f6e66ff2298be964922420e96ed0e8
ys10 <1269273556@qq.com>
2017-02-22 22:20:37 +0800

    Test how to read a HDF5 file.

--- src/test/H5pyTestDemo.py
+++ src/test/H5pyTestDemo.py
@@ -1,1 +1,17 @@
 import numpy, h5py
+import os
+
+# Current path.
+path = os.path.abspath('.')
+print(path)
+# Read HDF5 file as data set.
+f = h5py.File('../../tmp/data/train-timit.hdf5', 'r')
+print(f.keys())
+# Tensors as input data.
+x = f['source/fbcg1_si982_5']
+print(x.shape)
+print(x.dtype)
+# Label as expected classification result.
+y = f['target/fbcg1_si982_5']
+print(y.shape)
+print(y.dtype)
