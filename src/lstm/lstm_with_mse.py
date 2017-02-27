e3484b6b68d2cf09fab7d95cfe66b80a9de6a987
ys10 <1269273556@qq.com>
2017-02-27 22:06:13 +0800

    Modify the access model of log file.

--- src/lstm/lstm_with_mse.py
+++ src/lstm/lstm_with_mse.py
@@ -18,7 +18,7 @@ logging.basicConfig(level=logging.DEBUG,
                 format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
                 datefmt='%a, %d %b %Y %H:%M:%S',
                 filename='../../log/app.log',
-                filemode='w')
+                filemode='r')
 
 # Import configuration by config parser.
 cp = configparser.ConfigParser()
