4a88d16aa105f429cf6468f5a4fddcc5ef7e84b3
ys10 <1269273556@qq.com>
2017-03-02 21:15:56 +0800

    Modify the h5py demo to read ctc data.

--- src/test/H5pyTestDemo.py
+++ src/test/H5pyTestDemo.py
@@ -4,9 +4,9 @@ from math import ceil
 from numpy import array
 
 # File storing group name.
-group_file_name = '../../tmp/data/train_speechorder_timit.txt'
+group_file_name = '../../tmp/data/train_speechorder_timit_ctc.txt'
 # HDF5 file as training data set.
-training_data_file_name = '../../tmp/data/train-timit.hdf5'
+training_data_file_name = '../../tmp/data/train-timit-ctc.hdf5'
 # Current path.
 path = os.path.abspath('.')
 print(path)
@@ -16,18 +16,15 @@ groups = open(group_file_name, 'r');
 training_data = h5py.File(training_data_file_name, 'r')
 
 # Get a group.
-print(training_data.keys())
+print("keys: "+ str(training_data.keys()))
 # Tensors as input data.
-X = training_data['source/fbcg1_si982_5']
+X = training_data['source/falk0_si1086']
 print(X.shape)
 print(X.dtype)
 # Label as expected classification result.
-Y = training_data['target/fbcg1_si982_5']
+Y = training_data['target/falk0_si1086']
 print(Y.shape)
 print(Y.dtype)
-
-print("[X]:")
-print([X])
 
 batch_x = []
 batch_x.append(X)
@@ -39,6 +36,17 @@ print(batch_x)
 batch_x = array(batch_x)
 print("batch_x:")
 print(batch_x.shape)
+
+batch_y = []
+batch_y.append(Y)
+batch_y.append(Y)
+
+print("batch_y:")
+print(batch_y)
+
+batch_y = array(batch_y)
+print("batch_y:")
+print(batch_y.shape)
 
 
 batch_size = 32;
@@ -54,8 +62,8 @@ while 1:
         # Get training data by group name without line break.
         X = training_data['source/'+line.strip('\n')]
         Y = training_data['target/'+line.strip('\n')]
-        # for i in range(0, X.shape[0], 1):
-        #     print(X[i])
-        # for i in range(0, Y.shape[0], 1):
-        #     print(Y[i])
+        for i in range(0, X.shape[0], 1):
+            print(X[i])
+        for i in range(0, Y.shape[0], 1):
+            print(Y[i])
     break
