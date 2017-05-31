373e80c01e9b7c639cdb0086272a7e2e73e63e17
ys10 <1269273556@qq.com>
2017-05-31 15:35:22 +0800

    note break.

--- src/lstm/lstm_with_ctc_with_tensorboard.py
+++ src/lstm/lstm_with_ctc_with_tensorboard.py
@@ -73,8 +73,8 @@ we will then handle 69 dimension sequences of 200 steps for every sample.
 with tf.name_scope('parameter'):
     base_learning_rate = tf.Variable(0.0001, trainable=False)
     learning_rate = base_learning_rate
-    start_epoch = 0
-    end_epoch = 60
+    start_epoch = 40
+    end_epoch = 100
     decay_epoch = 15
     reset_epoch = 30
     decay_rate = 0.95
@@ -340,7 +340,7 @@ for epoch in range(start_epoch, end_epoch, 1):
                           + ", Batch beam ler= {:.6f}".format(batch_beam_ler) + ", Batch greddy ler= {:.6f}".format(batch_greedy_ler))
             logging.debug("beam decode:" + str(batch_beam_decode))
             logging.debug("greddy decode:" + str(batch_greedy_decode))
-        break;
+        # break;
     # Metrics mean
     train_cost /= (batch_size * training_batches)
     train_beam_ler /= (batch_size * training_batches)
@@ -434,7 +434,7 @@ for epoch in range(start_epoch, end_epoch, 1):
                 batch_beam_ler) + ", Batch greddy ler= {:.6f}".format(batch_greedy_ler))
             logging.debug("beam decode:" + str(batch_beam_decode))
             logging.debug("greddy decode:" + str(batch_greedy_decode))
-        break;
+        # break;
     # Metrics mean
     validation_cost /= (batch_size * validation_batches)
     validation_beam_ler /= (batch_size * validation_batches)
